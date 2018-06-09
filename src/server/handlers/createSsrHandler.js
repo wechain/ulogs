6d3cea8649afdbfcbe77303148137a2c763dc130
kizzbonez <kizzbonez@gmail.com>
2018-06-09 11:22:24 +0800

    update

--- src/server/handlers/createSsrHandler.js
+++ src/server/handlers/createSsrHandler.js
@@ -64,10 +64,10 @@ export default function createSsrHandler(template) {
         res.status(context.status);
       }
 
-      return res.send(renderSsrPage(store, content, assets, template, req.hostname !== 'busy.org'));
+      return res.send(renderSsrPage(store, content, assets, template, req.hostname !== 'ulogs.org'));
     } catch (err) {
       console.error('SSR error occured, falling back to bundled application instead', err);
-      return res.send(renderSsrPage(null, null, assets, template, req.hostname !== 'busy.org'));
+      return res.send(renderSsrPage(null, null, assets, template, req.hostname !== 'ulogs.org'));
     }
   };
 }
