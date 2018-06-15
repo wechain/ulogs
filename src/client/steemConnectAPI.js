ac457aebb3538ff46974d8642e67133bdea28155
kizzbonez <kizzbonez@gmail.com>
2018-06-15 08:28:49 +0800

    update

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -3,7 +3,7 @@ import sc2 from 'sc2-sdk';
 const api = sc2.Initialize({
   app: 'ulogs.app', //process.env.STEEMCONNECT_CLIENT_ID
   baseURL: process.env.STEEMCONNECT_HOST,  //process.env.r
-  callbackURL: 'http://ulogs.org/callback', // process.env.STEEMCONNECT_REDIRECT_URL
+  callbackURL: 'https://www.ulogs.org/callback', // process.env.STEEMCONNECT_REDIRECT_URL
 });
 
 export default api;
